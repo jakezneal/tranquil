03204968498fbe5cc33bc7c4b228e298c735220c
Jake Neal <hello@jakeneal.co.uk>
2020-08-05 13:48:27 +0100

    :sparkles: Set up Twitter and Open Graph

--- nuxt.config.js
+++ nuxt.config.js
@@ -18,6 +18,9 @@ export default {
    ** See https://nuxtjs.org/api/configuration-head
    */
   head: {
+    htmlAttrs: {
+      lang: 'en',
+    },
     title: pkg.headMeta.title,
     meta: [
       { charset: 'utf-8' },
@@ -25,8 +28,37 @@ export default {
       {
         hid: 'description',
         name: 'description',
+        itemprop: 'description',
         content: pkg.headMeta.description,
       },
+      { name: 'image', content: pkg.headMeta.imageUrl },
+      { itemprop: 'name', content: pkg.headMeta.title },
+      { itemprop: 'image', content: pkg.headMeta.imageUrl },
+      { name: 'twitter:card', content: 'summary' },
+      {
+        hid: 'twitter:title',
+        name: 'twitter:title',
+        content: pkg.headMeta.title,
+      },
+      {
+        hid: 'twitter:description',
+        name: 'twitter:description',
+        content: pkg.headMeta.description,
+      },
+      { name: 'twitter:site', content: pkg.headMeta.twitter },
+      { name: 'twitter:creator', content: pkg.headMeta.twitter },
+      { name: 'twitter:image:src', content: pkg.headMeta.imageUrl },
+      { hid: 'og:title', name: 'og:title', content: pkg.headMeta.title },
+      {
+        hid: 'og:description',
+        name: 'og:description',
+        content: pkg.headMeta.description,
+      },
+      { name: 'og:image', content: pkg.headMeta.imageUrl },
+      { hid: 'og:url', name: 'og:url', content: pkg.headMeta.siteUrl },
+      { name: 'og:site_name', content: pkg.headMeta.title },
+      { name: 'og:locale', content: 'en_GB' },
+      { name: 'og:type', content: 'website' },
     ],
     link: [{ rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }],
   },
